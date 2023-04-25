636d9f8c91ab5174e215aed9f6837aaf3f2b6a7d
alxndrsn <alxndrsn>
2023-04-25 08:05:29 +0000

    Remove unnecessary marker

--- tests/integration/browser.migration.js
+++ tests/integration/browser.migration.js
@@ -4,10 +4,6 @@
 'use strict';
 
 describe('migration', function () {
-  it('should tell me which part of the matrix this runs on', () => {
-    throw new Error('hiiiiiiiii' + scenarios);
-  });
-
   function usingDefaultPreferredAdapters() {
     console.log('preferredAdapters: ' + PouchDB.preferredAdapters);
     return true;
