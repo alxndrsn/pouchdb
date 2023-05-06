91bd675da615e9998d01efa8c51e694e50e99036
alxndrsn <alxndrsn>
2023-05-06 09:15:01 +0000

    build-modules: log node version

--- bin/build-modules.js
+++ bin/build-modules.js
@@ -16,7 +16,7 @@ function buildPackage(pkg) {
     if (!stat.isDirectory()) { // skip e.g. 'npm-debug.log'
       return;
     }
-    console.log('Building ' + pkg + '...');
+    console.log('Building ' + pkg + 'with node version ' + process.version + '...');
     if (pkg === 'pouchdb') {
       return buildPouchDB();
     } else {
