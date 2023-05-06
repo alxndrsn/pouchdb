4fada17ec22396d0536e7289e76d0848eba220e2
alxndrsn <alxndrsn>
2023-05-06 10:26:03 +0000

    revert more changes

--- bin/build-module.js
+++ bin/build-module.js
@@ -36,13 +36,11 @@ var BROWSER_DEPENDENCY_ONLY_PACKAGES =
 
 function buildModule(filepath) {
   var pkg = require(path.resolve(filepath, 'package.json'));
+  var topPkg = require(path.resolve(filepath, '../../../package.json'));
   var pouchdbPackages = fs.readdirSync(path.resolve(filepath, '..'));
   // All external modules are assumed to be CommonJS, and therefore should
   // be skipped by Rollup. We may revisit this later.
-  var depsToSkip = fs
-    .readdirSync(path.resolve(filepath, '../../../node_modules'), { withFileTypes:true })
-    .filter(it => it.isDirectory())
-    .map(it => it.name)
+  var depsToSkip = Object.keys(topPkg.dependencies || {})
     .concat(builtInModules);
 
   if (AGGRESSIVELY_BUNDLED_PACKAGES.indexOf(pkg.name) === -1) {
