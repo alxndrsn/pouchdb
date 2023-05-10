93791627d82ce170889bc4ceaa1abf049b8be91a
alxndrsn <alxndrsn>
2023-05-10 20:32:47 +0000

    Make webpack test fail faster

--- bin/test-webpack.sh
+++ bin/test-webpack.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash -eux
 
 #
 # Build PouchDB with Webpack instead of Browserify, and test that.
@@ -7,7 +7,7 @@
 #
 
 npm run build
-npm i webpack@5.66.0 -D webpack-cli@4.9.2 # do this on-demand to avoid slow installs
+npm install webpack-cli@5.1.1 # do this on-demand to avoid slow installs
 node bin/update-package-json-for-publish.js
-./node_modules/.bin/webpack
-BUILD_NODE_DONE=0 POUCHDB_SRC='../../pouchdb-webpack.js' npm test
+npx webpack-cli
+BUILD_NODE_DONE=1 POUCHDB_SRC='../../pouchdb-webpack.js' npm test
