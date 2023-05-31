fb195d3a13dc626306ffaf9978bf2e42a016da10
alxndrsn <alxndrsn>
2023-05-31 06:30:22 +0000

    Add error names

--- tests/integration/test.viewadapter.js
+++ tests/integration/test.viewadapter.js
@@ -137,7 +137,7 @@ viewAdapters.forEach(viewAdapter => {
               // Something is saved here
               // This shows that without a view_adapter specified
               // the view query data is stored in the default adapter database.
-              viewRequest.result.objectStoreNames.length.should.equal(7);
+              viewRequest.result.objectStoreNames.length.should.equal(7, 'viewRequest');
 
               // check indexedDB for saved docs
               const docRequest = indexedDB.open(docDbName, 5);
@@ -146,7 +146,7 @@ viewAdapters.forEach(viewAdapter => {
               };
               docRequest.onsuccess = function () {
                 // something is saved here
-                docRequest.result.objectStoreNames.length.should.equal(7);
+                docRequest.result.objectStoreNames.length.should.equal(7, 'docRequest');
                 done();
               };
             };
