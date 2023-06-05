f04f7ad13bea514c18caea590da8802532450e07
alxndrsn <alxndrsn>
2023-06-05 09:46:37 +0000

    Log view generation

--- tests/integration/test.viewadapter.js
+++ tests/integration/test.viewadapter.js
@@ -49,6 +49,9 @@ viewAdapters.forEach(viewAdapter => {
     });
 
     it('Create pouch with separate view adapters', function (done) {
+      global.__magic_number = Math.random();
+      console.log('Opening PouchDB with __magic_number:', global.__magic_number);
+
       const db = new PouchDB(dbs.name, {view_adapter: viewAdapter});
 
       if (db.adapter === viewAdapter) {
