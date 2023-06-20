5759c397ee533d4b88dd380fd28c7fd41a9671c0
alxndrsn <alxndrsn>
2023-06-20 08:38:40 +0000

    revert random changes

--- tests/integration/test.replication.js
+++ tests/integration/test.replication.js
@@ -2147,7 +2147,6 @@ adapters.forEach(function (adapters) {
       if (testUtils.isIE()) {
         return done();
       }
-
       var db = new PouchDB(dbs.name);
       var remote = new PouchDB(dbs.remote);
       var docs = [];
