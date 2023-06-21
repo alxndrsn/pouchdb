10f31d7ef0c0e45a96c9facf223798cf5098766a
alxndrsn <alxndrsn>
2023-06-21 07:01:46 +0000

    browser-info: re-instate tests and add deliberate failure

--- tests/integration/browser.info.js
+++ tests/integration/browser.info.js
@@ -1,3 +1,42 @@
 'use strict';
 
-throw new Error('just saying hi');
+var adapters = ['local'];
+
+adapters.forEach(function (adapter) {
+  describe('browser.info.js-' + adapter, function () {
+
+    var dbs = {};
+
+    beforeEach(function (done) {
+      dbs.name = testUtils.adapterUrl(adapter, 'testdb');
+      testUtils.cleanup([dbs.name], done);
+    });
+
+    after(function (done) {
+      testUtils.cleanup([dbs.name], done);
+    });
+
+    it('should fail', function () {
+      throw new Error('i failed on purpose');
+    });
+
+    it('adapter-specific info', function () {
+      var db = new PouchDB(dbs.name);
+      return db.info().then(function (info) {
+        switch (db.adapter) {
+          case 'websql':
+            info.websql_encoding.should.be.a('string');
+            info.adapter.should.equal('websql');
+            break;
+          case 'idb':
+            info.idb_attachment_format.should.be.a('string');
+            info.adapter.should.equal('idb');
+            break;
+          default:
+            should.exist(info); // can't make any guarantees
+            break;
+        }
+      });
+    });
+  });
+});
