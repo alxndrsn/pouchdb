2027e499cb5851c27d59645212977cf4733cae11
alxndrsn <alxndrsn>
2023-06-21 08:46:51 +0000

    Tone-down window checks

--- tests/integration/browser.worker.js
+++ tests/integration/browser.worker.js
@@ -21,10 +21,7 @@ function runTests() {
   before(function () {
     worker = new Worker('worker.js');
 
-    var sourceFile = window &&
-        window.location &&
-        window.location.search &&
-        window.location.search.match(/[?&]sourceFile=([^&]+)/);
+    var sourceFile = window && window.location.search.match(/[?&]sourceFile=([^&]+)/);
 
     if (!sourceFile) {
       sourceFile = '../../packages/node_modules/pouchdb/dist/pouchdb.js';
