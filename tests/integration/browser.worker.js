f3f903023ce48a03b37df13f850b75e0f8c29c15
alxndrsn <alxndrsn>
2023-06-22 08:53:35 +0000

    Revert "deliberately fail tests to make sure they're actually running"
    This reverts commit f0730022c67462535559bb50853123aaa4afe47e.

--- tests/integration/browser.worker.js
+++ tests/integration/browser.worker.js
@@ -6,7 +6,21 @@ describe('browser.worker.js', function () {
   var dbs = {};
 
   before(function () {
-    throw new Error('deliberately fail');
+    if (typeof process !== 'undefined' && process.env.SKIP_WORKER_TEST === '1') {
+      this.skip();
+    }
+
+    worker = new Worker('worker.js');
+
+    var sourceFile = window && window.location.search.match(/[?&]sourceFile=([^&]+)/);
+
+    if (!sourceFile) {
+      sourceFile = '../../packages/node_modules/pouchdb/dist/pouchdb.js';
+    } else {
+      sourceFile = '../../packages/node_modules/pouchdb/dist/' + sourceFile[1];
+    }
+
+    worker.postMessage(['source', sourceFile]);
   });
 
   after(function () {
