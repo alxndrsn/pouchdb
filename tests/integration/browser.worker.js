8fff9647c4a6ce9147cf6426d5c181042187c1c6
alxndrsn <alxndrsn>
2023-06-22 08:23:50 +0000

    deliberately fail tests again

--- tests/integration/browser.worker.js
+++ tests/integration/browser.worker.js
@@ -17,21 +17,7 @@ describe('browser.worker.js', function () {
   }
 
   before(function () {
-    if (process && process.env.SKIP_WORKER_TEST === '1') {
-      this.skip();
-    }
-
-    worker = new Worker('worker.js');
-
-    var sourceFile = window && window.location.search.match(/[?&]sourceFile=([^&]+)/);
-
-    if (!sourceFile) {
-      sourceFile = '../../packages/node_modules/pouchdb/dist/pouchdb.js';
-    } else {
-      sourceFile = '../../packages/node_modules/pouchdb/dist/' + sourceFile[1];
-    }
-
-    worker.postMessage(['source', sourceFile]);
+    throw new Error('deliberately fail again');
   });
 
   after(function () {
