8b527ffba3f5e6e47e62e11dea131c062851726e
alxndrsn <alxndrsn>
2023-06-22 08:20:30 +0000

    Deliberately fail tests, to make sure they're running where expected

--- tests/integration/browser.worker.js
+++ tests/integration/browser.worker.js
@@ -7,21 +7,7 @@ function runTests() {
   var worker;
 
   before(function () {
-    if (process && process.env.SKIP_WORKER_TEST === '1') {
-      this.skip();
-    }
-
-    worker = new Worker('worker.js');
-
-    var sourceFile = window && window.location.search.match(/[?&]sourceFile=([^&]+)/);
-
-    if (!sourceFile) {
-      sourceFile = '../../packages/node_modules/pouchdb/dist/pouchdb.js';
-    } else {
-      sourceFile = '../../packages/node_modules/pouchdb/dist/' + sourceFile[1];
-    }
-
-    worker.postMessage(['source', sourceFile]);
+    throw new Error('deliberately fail');
   });
 
   after(function () {
