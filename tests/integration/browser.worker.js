0169b1497b8bc163fd2060a8f8e476c88343c3ce
alxndrsn <alxndrsn>
2023-06-22 08:24:07 +0000

    Revert "deliberately fail tests again"
    This reverts commit 8fff9647c4a6ce9147cf6426d5c181042187c1c6.

--- tests/integration/browser.worker.js
+++ tests/integration/browser.worker.js
@@ -17,7 +17,21 @@ describe('browser.worker.js', function () {
   }
 
   before(function () {
-    throw new Error('deliberately fail again');
+    if (process && process.env.SKIP_WORKER_TEST === '1') {
+      this.skip();
+    }
+
+    worker = new Worker('worker.js');
+
+    var sourceFile = window && window.location.search.match(/[?&]sourceFile=([^&]+)/);
+
+    if (!sourceFile) {
+      sourceFile = '../../packages/node_modules/pouchdb/dist/pouchdb.js';
+    } else {
+      sourceFile = '../../packages/node_modules/pouchdb/dist/' + sourceFile[1];
+    }
+
+    worker.postMessage(['source', sourceFile]);
   });
 
   after(function () {
