54e49d2412890c38b09491b05c40099f65cac488
alxndrsn <alxndrsn>
2023-06-22 08:21:44 +0000

    remove outer test wrapper

--- tests/integration/browser.worker.js
+++ tests/integration/browser.worker.js
@@ -1,129 +1,124 @@
 'use strict';
 
-runTests();
+var worker;
 
-function runTests() {
-
-  var worker;
-
-  before(function () {
-    if (process && process.env.SKIP_WORKER_TEST === '1') {
-      this.skip();
-    }
-
-    worker = new Worker('worker.js');
-
-    var sourceFile = window && window.location.search.match(/[?&]sourceFile=([^&]+)/);
-
-    if (!sourceFile) {
-      sourceFile = '../../packages/node_modules/pouchdb/dist/pouchdb.js';
-    } else {
-      sourceFile = '../../packages/node_modules/pouchdb/dist/' + sourceFile[1];
-    }
-
-    worker.postMessage(['source', sourceFile]);
-  });
-
-  after(function () {
-    worker.terminate();
-  });
-
-  function workerPromise(message) {
-    return new Promise(function (resolve, reject) {
-      worker.onerror = function (e) {
-        reject(new Error(e.message + ": " + e.filename + ': ' + e.lineno));
-      };
-      worker.onmessage = function (e) {
-        resolve(e.data);
-      };
-      worker.postMessage(message);
-    });
+before(function () {
+  if (process && process.env.SKIP_WORKER_TEST === '1') {
+    this.skip();
   }
 
-  describe('browser.worker.js', function () {
+  worker = new Worker('worker.js');
 
-    var dbs = {};
+  var sourceFile = window && window.location.search.match(/[?&]sourceFile=([^&]+)/);
 
-    beforeEach(function (done) {
-      dbs.name = testUtils.adapterUrl('local', 'testdb');
-      dbs.remote = testUtils.adapterUrl('http', 'test_repl_remote');
-      testUtils.cleanup([dbs.name, dbs.remote], done);
-    });
+  if (!sourceFile) {
+    sourceFile = '../../packages/node_modules/pouchdb/dist/pouchdb.js';
+  } else {
+    sourceFile = '../../packages/node_modules/pouchdb/dist/' + sourceFile[1];
+  }
 
-    after(function (done) {
-      testUtils.cleanup([dbs.name, dbs.remote], done);
-    });
+  worker.postMessage(['source', sourceFile]);
+});
 
-    it('create it', function () {
-      return workerPromise('ping').then(function (data) {
-        data.should.equal('pong');
-      });
-    });
+after(function () {
+  worker.terminate();
+});
 
-    it('check pouch version', function () {
-      return workerPromise('version').then(function (data) {
-        PouchDB.version.should.equal(data);
-      });
-    });
-
-    it('create remote db', function () {
-      return workerPromise(['create', dbs.remote]).then(function (data) {
-        data.should.equal('lala');
-      });
-    });
-
-    it('create local db', function () {
-      return workerPromise(['create', dbs.name]).then(function (data) {
-        data.should.equal('lala');
-      });
-    });
-
-    it('add doc with blob attachment', function () {
-      return workerPromise(['postAttachmentThenAllDocs', dbs.name]).then(function (data) {
-        data.title.should.equal('lalaa');
-      });
-    });
-
-    it('put an attachment', function () {
-      var blob = new Blob(['foobar'], {type: 'text/plain'});
-      var message = ['putAttachment', dbs.name, 'doc', 'att.txt', blob,
-        'text/plain'];
-      return workerPromise(message).then(function (blob) {
-        blob.type.should.equal('text/plain');
-        blob.size.should.equal(6);
-      });
-    });
-
-    it('total_rows consistent between worker and main thread', function () {
-      var db = new PouchDB(dbs.name);
-
-      // this test only makes sense for idb
-      if (db.adapter !== 'idb') {
-        return;
-      }
-
-      // both threads agree the count is 0
-      return testUtils.Promise.all([
-        db.allDocs().then(function (res) {
-          res.total_rows.should.equal(0);
-        }),
-        workerPromise(['allDocs', dbs.name]).then(function (res) {
-          res.total_rows.should.equal(0);
-        })
-      ]).then(function () {
-        // post a doc
-        return db.post({});
-      }).then(function () {
-        // both threads agree the count is 1
-        return testUtils.Promise.all([
-          db.allDocs().then(function (res) {
-            res.total_rows.should.equal(1);
-          }),
-          workerPromise(['allDocs', dbs.name]).then(function (res) {
-            res.total_rows.should.equal(1);
-          })
-        ]);
-      });
-    });
+function workerPromise(message) {
+  return new Promise(function (resolve, reject) {
+    worker.onerror = function (e) {
+      reject(new Error(e.message + ": " + e.filename + ': ' + e.lineno));
+    };
+    worker.onmessage = function (e) {
+      resolve(e.data);
+    };
+    worker.postMessage(message);
   });
 }
+
+describe('browser.worker.js', function () {
+
+  var dbs = {};
+
+  beforeEach(function (done) {
+    dbs.name = testUtils.adapterUrl('local', 'testdb');
+    dbs.remote = testUtils.adapterUrl('http', 'test_repl_remote');
+    testUtils.cleanup([dbs.name, dbs.remote], done);
+  });
+
+  after(function (done) {
+    testUtils.cleanup([dbs.name, dbs.remote], done);
+  });
+
+  it('create it', function () {
+    return workerPromise('ping').then(function (data) {
+      data.should.equal('pong');
+    });
+  });
+
+  it('check pouch version', function () {
+    return workerPromise('version').then(function (data) {
+      PouchDB.version.should.equal(data);
+    });
+  });
+
+  it('create remote db', function () {
+    return workerPromise(['create', dbs.remote]).then(function (data) {
+      data.should.equal('lala');
+    });
+  });
+
+  it('create local db', function () {
+    return workerPromise(['create', dbs.name]).then(function (data) {
+      data.should.equal('lala');
+    });
+  });
+
+  it('add doc with blob attachment', function () {
+    return workerPromise(['postAttachmentThenAllDocs', dbs.name]).then(function (data) {
+      data.title.should.equal('lalaa');
+    });
+  });
+
+  it('put an attachment', function () {
+    var blob = new Blob(['foobar'], {type: 'text/plain'});
+    var message = ['putAttachment', dbs.name, 'doc', 'att.txt', blob,
+      'text/plain'];
+    return workerPromise(message).then(function (blob) {
+      blob.type.should.equal('text/plain');
+      blob.size.should.equal(6);
+    });
+  });
+
+  it('total_rows consistent between worker and main thread', function () {
+    var db = new PouchDB(dbs.name);
+
+    // this test only makes sense for idb
+    if (db.adapter !== 'idb') {
+      return;
+    }
+
+    // both threads agree the count is 0
+    return testUtils.Promise.all([
+      db.allDocs().then(function (res) {
+        res.total_rows.should.equal(0);
+      }),
+      workerPromise(['allDocs', dbs.name]).then(function (res) {
+        res.total_rows.should.equal(0);
+      })
+    ]).then(function () {
+      // post a doc
+      return db.post({});
+    }).then(function () {
+      // both threads agree the count is 1
+      return testUtils.Promise.all([
+        db.allDocs().then(function (res) {
+          res.total_rows.should.equal(1);
+        }),
+        workerPromise(['allDocs', dbs.name]).then(function (res) {
+          res.total_rows.should.equal(1);
+        })
+      ]);
+    });
+  });
+});
