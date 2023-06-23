4a1d033687f96be873b1a04335606c046df9bdb4
alxndrsn <alxndrsn>
2023-06-23 07:52:22 +0000

    tell us if it would have run

--- tests/integration/browser.worker.js
+++ tests/integration/browser.worker.js
@@ -1,6 +1,21 @@
 'use strict';
 
 describe('browser.worker.js', function () {
+  it('might run', function () {
+    var isNodeWebkit = typeof window !== 'undefined' &&
+      typeof process !== 'undefined';
+
+    var wouldRun = false;
+
+    if ((window && typeof window.Worker === 'function') &&
+        !isNodeWebkit && !testUtils.isIE() &&
+        ((window && window.chrome) || (navigator && /Firefox/.test(navigator.userAgent)))) {
+      wouldRun = true;
+    }
+
+    wouldRun.should.equal('the truth');
+  });
+
   it('should inform us of useful information', function () {
     const env = {
       'window': typeof window,
