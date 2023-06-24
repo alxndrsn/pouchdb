4c7ca41102c46b60b11979c7e275aa56337151a9
alxndrsn <alxndrsn>
2023-06-24 16:17:20 +0000

    sync_events: shorter timeout

--- tests/integration/test.sync_events.js
+++ tests/integration/test.sync_events.js
@@ -23,6 +23,7 @@ adapters.forEach(function (adapters) {
     });
 
     it('#4251 Should fire paused and active on sync', function (done) {
+      this.timeout(1000);
       try {
         var db = new PouchDB(dbs.name);
         var remote = new PouchDB(dbs.remote);
