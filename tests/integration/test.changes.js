8d169e15f8e6d28b83941884727d00714c93d379
alxndrsn <alxndrsn>
2023-06-25 17:16:59 +0000

    remove mixed promise/done

--- tests/integration/test.changes.js
+++ tests/integration/test.changes.js
@@ -653,30 +653,32 @@ adapters.forEach(function (adapter) {
       });
     });
 
-    it('Changes with invalid ddoc view name', async function (done) {
+    it('Changes with invalid ddoc view name', function (done) {
       var db = new PouchDB(dbs.name);
 
-      await new Promise(resolve => setTimeout(resolve, 100));
-      // TODO add sleep because below will sometimes fail with 'Database does not exist':
-      // see: https://github.com/alxndrsn/pouchdb/actions/runs/5370708031/jobs/9742995589
-      // Perhaps it's a race condition?
+      new Promise(resolve => setTimeout(resolve, 100))
+        .then(() => {
+          // TODO add sleep because below will sometimes fail with 'Database does not exist':
+          // see: https://github.com/alxndrsn/pouchdb/actions/runs/5370708031/jobs/9742995589
+          // Perhaps it's a race condition?
 
-      db.post({}).catch(err => {
-        err.message = 'db.post() failed; db.name='+db.name+'; ' + err.message;
-        done(err);
-      });
-      var changes = db.changes({live: true, filter: '_view', view: ''});
-      changes.on('error', (err) => {
-        try {
-          err.name.should.equal('bad_request');
-          done();
-        } catch (error) {
-          console.log('Calling done() with:', error); // TODO just for debugging?
-          done(error);
-        }
-      });
-      changes.on('complete', () => done(new Error('Unexpected event: "complete"')));
-      changes.on('change', () => done(new Error('Unexpected event: "change"')));
+          db.post({}).catch(err => {
+            err.message = 'db.post() failed; db.name='+db.name+'; ' + err.message;
+            done(err);
+          });
+          var changes = db.changes({live: true, filter: '_view', view: ''});
+          changes.on('error', (err) => {
+            try {
+              err.name.should.equal('bad_request');
+              done();
+            } catch (error) {
+              console.log('Calling done() with:', error); // TODO just for debugging?
+              done(error);
+            }
+          });
+          changes.on('complete', () => done(new Error('Unexpected event: "complete"')));
+          changes.on('change', () => done(new Error('Unexpected event: "change"')));
+        });
     });
 
     it('Changes with invalid ddoc view name 2', function (done) {
