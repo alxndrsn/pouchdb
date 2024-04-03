09c15f129c85aeffe1364b9bca7c1ff6266c9fb5
alxndrsn <alxndrsn>
2024-04-03 11:34:04 +0000

    add option to sort adapters

--- bin/perf-test-results/compare-multiple.js
+++ bin/perf-test-results/compare-multiple.js
@@ -15,6 +15,7 @@ const clients = {};
 const clientFilter  = process.env.CLIENT;
 const adapterFilter = process.env.ADAPTERS && process.env.ADAPTERS.split(',');
 const gitFilter     = process.env.COMMITS  && process.env.COMMITS.split(',').map(commit => commit.substring(0, 7));
+const sortOrder     = process.env.SORT?.split(',') || [];
 
 const filteredResults = rawResults.filter(({ adapter, client, results }) => {
   if (adapterFilter) {
@@ -76,7 +77,16 @@ if (adapters.length < 2) {
   process.exit(1);
 }
 
-adapters.sort();
+adapters.sort((a, b) => {
+  if(a === b) return 0;
+
+  const requestedA = sortOrder.indexOf(a);
+  const requestedB = sortOrder.indexOf(b);
+
+  if(requestedA !== requestedB) return requestedA - requestedB;
+  if(a < b) return -1;
+  return 1;
+});
 
 const sortedResults = adapters.map(adapter => ({
   adapter, results:resultsByAdapter[adapter]
